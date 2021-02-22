bd268582897b170ae94578ba2f56ea12ea29525f
MauricioMacedoVillarnobo <mm.villarnobo@gmail.com>
2021-02-22 01:45:50 -0300

    compartimentalização

--- pages/index.js
+++ pages/index.js
@@ -4,30 +4,38 @@ function Home()
 {
     return (<div>
 
-        <iframe width="420" height="315"
-        src="https://www.youtube.com/embed/tgbNymZ7vqY">
-        </iframe>
+        <div>
+            <iframe width="420" height="315"
+            src="https://www.youtube.com/embed/tgbNymZ7vqY">
+            </iframe>
+        </div>
 
-        <br></br>
-        <hr></hr>
-        <br></br>
+        <div>
+            <br></br>
+            <hr></hr>
+            <br></br>
+        </div>
 
-        <h1>Formação</h1>
-        <p>PUC-SP</p>
-        <p>SI</p>
-        <p>Saga</p>
-        <p>SuperGeeks</p>
+        <div>
+            <h1>Formação</h1>
+            <p>PUC-SP</p>
+            <p>SI</p>
+            <p>Saga</p>
+            <p>SuperGeeks</p>
+        </div>
 
-        <br></br>
-        <h1>Competência</h1>
-        <p>Inglês Avançado</p>
-        <p>Linguagem de Programação C/C++ básico</p>
-        <p>Microsoft Visual Studio</p>
+        <div>
+            <h1>Competência</h1>
+            <p>Inglês Avançado</p>
+            <p>Linguagem de Programação C/C++ básico</p>
+            <p>Microsoft Visual Studio</p>
+        </div>
 
-        <br></br>
-        <Link href="/saibaMais">
-        <a><h1>Saiba Mais +</h1></a>
-        </Link>
+        <div>
+            <Link href="/saibaMais">
+            <a><h1>Saiba Mais +</h1></a>
+            </Link>
+        </div>
         
     </div>)
 }
