412831054c196eafc25a110a7f4555b272f550a9
MauricioMacedoVillarnobo <mm.villarnobo@gmail.com>
2021-02-22 01:08:19 -0300

    layout teste

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,34 @@ function Home()
             <a>Acessar página Sobre</a>
         </Link>
 
+        <br></br>
         <hr></hr>
+        <br></br>
+
         <iframe width="420" height="315"
         src="https://www.youtube.com/embed/tgbNymZ7vqY">
         </iframe>
+
+        <br></br>
+        <hr></hr>
+        <br></br>
+
+        <h1><b>Formação</b></h1>
+        <p>PUC-SP</p>
+        <p>SI</p>
+        <p>Saga</p>
+        <p>SuperGeeks</p>
+
+        <br></br>
+        <h1>Competência</h1>
+        <p>Inglês Avançado</p>
+        <p>Linguagem de Programação C/C++</p>
+        <p>Microsoft Visual Studio</p>
+
+        <br></br>
+        <h1>Saiba Mais</h1>
+
+
         
     </div>)
 }
