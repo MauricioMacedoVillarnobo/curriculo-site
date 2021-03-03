cb16f23685d50ac847a12a36669e9f3e562d14b7
MauricioMacedoVillarnobo <mm.villarnobo@gmail.com>
2021-03-03 07:50:43 -0300

    mudancas4

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
 
     return <Component {...pageProps} />
 }
